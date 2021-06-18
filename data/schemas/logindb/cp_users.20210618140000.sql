aa1f2488b1c4c2ec4e765bce3bdd29cb9e62c416
Balfear <balfear@yandex.ru>
2021-06-18 16:23:28 +0300

    Change cp_users

--- data/schemas/logindb/cp_users.20210618140000.sql
+++ data/schemas/logindb/cp_users.20210618140000.sql
@@ -1,1 +1,21 @@
-ALTER TABLE `cp_users` CHANGE COLUMN `id` `user_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+CREATE TABLE IF NOT EXISTS `cp_users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(39) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `group_id` tinyint(3) NOT NULL default '0',
+  `birth_date` date NOT NULL,
+  `last_ip` varchar(100) NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `confirmed_date` datetime DEFAULT NULL,
+  `confirm_code` varchar(32) DEFAULT NULL,
+  `confirm_expire` datetime DEFAULT NULL,
+  `unban_date` datetime DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE INDEX master_email
+ON cp_users (email);
