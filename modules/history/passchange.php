0b7ecb43f17b63401a22d2a7ac56cf5e240c82ba
ecdarreola <ecdarreola@gmail.com>
2021-06-20 16:55:57 +0800

    Fixed view for password changes or reset which includes user_id or account_id.

--- modules/history/passchange.php
+++ modules/history/passchange.php
@@ -6,17 +6,17 @@ $this->loginRequired();
 $title = Flux::message('HistoryPassChangeTitle');
 $passwordChangeTable = Flux::config('FluxTables.ChangePasswordTable');
 
-$sql = "SELECT COUNT(id) AS total FROM {$server->loginDatabase}.$passwordChangeTable WHERE account_id = ?";
+$sql = "SELECT COUNT(id) AS total FROM {$server->loginDatabase}.$passwordChangeTable WHERE account_id = ? OR user_id = ?";
 $sth = $server->connection->getStatement($sql);
-$sth->execute(array($session->account->account_id));
+$sth->execute(array($session->account->account_id, $session->account->user_id));
 
 $paginator = $this->getPaginator($sth->fetch()->total);
 $paginator->setSortableColumns(array('change_date', 'change_ip'));
 
-$sql = "SELECT change_date, change_ip FROM {$server->loginDatabase}.$passwordChangeTable WHERE account_id = ?";
+$sql = "SELECT change_date, change_ip FROM {$server->loginDatabase}.$passwordChangeTable WHERE account_id = ? OR user_id = ?";
 $sql = $paginator->getSQL($sql);
 $sth = $server->connection->getStatement($sql);
-$sth->execute(array($session->account->account_id));
+$sth->execute(array($session->account->account_id, $session->account->user_id));
 
 $changes = $sth->fetchAll();
 ?>
