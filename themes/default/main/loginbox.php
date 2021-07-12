0dfac3ebb51034b1cdc29a19c3b34ae4c2a6c8bd
Balfear <balfear@yandex.ru>
2021-07-12 15:59:50 +0300

    Allow to choose server without auth
    Moved switcher from loginbox to footer.

--- themes/default/main/loginbox.php
+++ themes/default/main/loginbox.php
@@ -18,19 +18,6 @@ if ($session->isLoggedIn()):
 			</a>
 		</strong>
 		on <?php echo htmlspecialchars($session->serverName) ?>.
-		
-	<?php if (count($athenaServerNames=$session->getAthenaServerNames()) > 1): ?>
-		Your preferred server is:
-	
-	<select name="preferred_server" onchange="updatePreferredServer(this)"<?php if (count($athenaServerNames=$session->getAthenaServerNames()) === 1) echo ' disabled="disabled"'  ?>>
-		<?php foreach ($athenaServerNames as $serverName): ?>
-		<option value="<?php echo htmlspecialchars($serverName) ?>"<?php if ($server->serverName == $serverName) echo ' selected="selected"' ?>><?php echo htmlspecialchars($serverName) ?></option>
-		<?php endforeach ?>
-	</select>.
-	<?php endif ?>
-	<form action="<?php echo $this->urlWithQs ?>" method="post" name="preferred_server_form" style="display: none">
-		<input type="hidden" name="preferred_server" value="" />
-	</form>
 	</span>
 	<?php if (!empty($adminMenuItems) && Flux::config('AdminMenuNewStyle')): ?>
 	<?php $mItems = array(); foreach ($adminMenuItems as $menuItem) $mItems[] = sprintf('<a href="%s">%s</a>', $menuItem['url'], htmlspecialchars(Flux::message($menuItem['name']))) ?>
