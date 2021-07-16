1a181bde748e31f70d63a74e83a68f65d8126059
Balfear <balfear@yandex.ru>
2021-07-16 14:03:07 +0300

    Update application.php

--- config/application.php
+++ config/application.php
@@ -117,7 +117,6 @@ return array(
 		//'admin3@localhost'								// -- because your Business Email is also checked.
 	),
 	'PaypalHackNotify'          => true,                    // Send email notification if hack attempt detected (Notification will be send for each address in list PayPalBusinessEmail and PayPalReceiverEmails)
-	'PayPalAllowedHosts'		=> array(
 	'PayPalAllowedHosts'		=> array(					// PayPal IP list https://www.paypal.com/fm/smarthelp/article/what-are-the-ip-addresses-for-live-paypal-servers-ts1056
 		'ipn.sandbox.paypal.com',
 		'notify.paypal.com'
