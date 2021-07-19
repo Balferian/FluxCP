0851c26187b6c2942d16c8df5f7275b4be365d8e
Balfear <balfear@yandex.ru>
2021-07-19 07:55:53 +0300

    Update storages.php

--- config/storages.php
+++ config/storages.php
@@ -2,7 +2,7 @@
 //table name => (Storage name, type ('account' or 'master'))
 return array(
 	'storage'			=> array('Standart', 'account'),
-	'storage_vip'		=> array('Premium', 'account'),
-	'storage_master'	=> array('Master', 'master'),
+	//'premium_storage'		=> array('Premium', 'account'),
+	//'storage_master'	=> array('Master', 'master'),
 )
 ?>
